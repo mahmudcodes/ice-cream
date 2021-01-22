d86c2640134cb6dfe781973453f653beb958d7d5
next-mahmud <54480128+next-mahmud@users.noreply.github.com>
2021-01-22 21:27:08 +0600

    change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './hope.svg';
 import './App.css';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Ice Cream
         </a>
       </header>
     </div>
